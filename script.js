cbf05bc8251ea03a0086d5a6b4846d7894663018
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-10-23 16:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-//your code here
+let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+let updatedList = arr.map((element) => {
+	// ["a", "some", "one"]
+	let words = element.split(" ") ;
+	let updatedString = words.reduce((prev, current, index) => {
+		if(current != "The" && current != "an" && current != "a"){
+			return prev + " " + current ;
+		}
+		return prev ;
+	}, "")
+
+	return updatedString ;
+}) ;
+let mp = {} ; // {"some one" : "a some one"}
+updatedList.forEach( (element, index) => {
+	mp[element] = arr[index] ;
+});
+// ["some one" , "mno abc" , "abc kh"]
+
+updatedList.sort(); 
+
+let finalAns = updatedList.map((element) => {
+    return mp[element] ;
+})
+
+const ul = document.getElementById("band")
+
+finalAns.forEach((item) => {
+	const li = document.createElement("li");
+	li.innerText = item ;
+	ul.append(li);
+})
+
 
